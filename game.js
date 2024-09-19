8182ead4d6936694b8309dd27b072d424e94505c
Andrii Vlasov <vlasov.andrii@gmail.com>
2024-09-19 21:38:54 +0200

    cosmetic changes

--- game.js
+++ game.js
@@ -6,8 +6,6 @@ const FONT_SIZE = 20;
 const LINE_SPACING = 10;
 const FALLING_SPEED = 8 / 10; // Adjust for desired difficulty
 
-
-
 // Set canvas to full screen
 const canvas = document.getElementById("myCanvas");
 canvas.width = window.innerWidth - 15;
@@ -16,7 +14,6 @@ canvas.height = window.innerHeight - 80;
 const MIN_X = 50; // Adjust according to your canvas size
 const MAX_X = canvas.width - 250; // Consider text width
 let randomX = MIN_X;
-
 
 // Initialize drawing context
 const ctx = canvas.getContext("2d");
@@ -31,6 +28,7 @@ wrongSound.volume = 0.1;
 
 let pressTimeout = 10;
 isGameRunning = true;
+isGameStarted = false;
 
 //programm to type
 let lines = [
@@ -48,7 +46,7 @@ let lines = [
 
 // Game variables
 let currentFallingLine = 0;
-let currentLineToType = 0;
+let lineNum = 0;
 //let currentLineY = -FONT_SIZE;
 let score = 0;
 let lineYs = [];
@@ -61,16 +59,15 @@ for (let i = 0; i < lines.length; i++) {
 }
 
 function typedCorrect() {
-	lineYs[currentLineToType] += 500;
+	lineYs[lineNum] += 500;
 	score++;
 	document.getElementById("score").textContent = `Score: ${score}`;
-	currentLineToType++;
-
+	lineNum++;
 	correctSound.play();
 
 	document.getElementById("userInput").value = ""; // Clear input
 	// Handle end of lines
-	if (currentLineToType === lines.length) {
+	if (lineNum === lines.length) {
 		// You can add a win message here or restart the game
 		console.log("Congratulations! You typed all the lines correctly.");
 		displayWinAnimation();
@@ -82,13 +79,19 @@ function typedCorrect() {
 
 // Game loop
 function animate() {
+	if (!isGameStarted) {
+		displayStartingScreen();
+		return;
+	}
+
 	if (!isGameRunning) return;
 	requestAnimationFrame(animate);
 	pressTimeout++;
+
 	// Make lines fall one by one
 	for (let i = 0; i < lineYs.length; i++) {
 		if (i == currentFallingLine) {
-			if (lineYs[i] < canvas.height - 15 * (i + 1 - currentLineToType)) {
+			if (lineYs[i] < canvas.height - 15 * (i + 1 - lineNum)) {
 				lineYs[i] += FALLING_SPEED;
 			} else {
 				currentFallingLine++;
@@ -107,22 +110,12 @@ function animate() {
 		// Draw the line at its calculated X and Y position
 		ctx.fillText(lines[i], randomXs[i], lineYs[i]);
 	}
-
-	document.addEventListener("keypress", function (event) {
-		if (event.key === "Enter" && (pressTimeout > 10)) {
-			handleUserInput()
-		}
-	});
-	document.getElementById('submitButton').addEventListener("click", function (event) {
-		if ((pressTimeout > 10))
-			handleUserInput()
-	});
 }
 
 function handleUserInput() {
 	// Handle user input checking here 
 	let userInput = document.getElementById("userInput").value.replace(/\s+/g, '');
-	if (userInput == lines[currentLineToType].replace(/\s+/g, '')) {
+	if (userInput == lines[lineNum].replace(/\s+/g, '')) {
 		typedCorrect();
 
 		pressTimeout = 0;
@@ -130,9 +123,16 @@ function handleUserInput() {
 		pressTimeout = 0;
 		wrongSound.play();
 		console.log(" input:", userInput);
-		console.log(" line to type:", lines[currentLineToType]);
-
+		console.log(" line to type:", lines[lineNum]);
 	}
+}
+
+function displayStartingScreen() {
+	ctx.fillStyle = BACKGROUND_COLOR;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = TEXT_COLOR;
+	ctx.font = "30px Arial";
+	ctx.fillText("Press Enter to Start", canvas.width / 2 - 100, canvas.height / 2);
 }
 
 function displayWinAnimation() {
@@ -163,9 +163,25 @@ function displayWinAnimation() {
 
 		fontSize += increment;
 	}
-
 	animateWinText();
 }
+
+
+document.addEventListener("keypress", function (event) {
+	if (isGameStarted) {
+		if (event.key === "Enter" && (pressTimeout > 10))
+			handleUserInput()
+	} else {
+		isGameStarted = true;
+		animate();
+	}
+});
+
+document.getElementById('submitButton').addEventListener("click", function (event) {
+	if ((pressTimeout > 10) && (isGameStarted))
+		handleUserInput()
+	else { isGameStarted = true; animate(); }
+});
 
 
 animate();
